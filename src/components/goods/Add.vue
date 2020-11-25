ef64ac7d343cc83e7d7e4fd831e89c0aed2dd80b
xujintai-jt <1791639435@qq.com>
2020-11-25 21:29:54 +0800

    改进

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -22,14 +22,8 @@
       </el-steps>
       <!-- 侧边标签页区域 -->
       <!-- 侧边栏表单 -->
-      <el-form
-        label-position="top"
-        :model="goodsAddForm"
-        :rules="goodsAddRules"
-        ref="goodsAddForm"
-        label-width="100px"
-        class="goodsAddForm"
-      >
+      <el-form label-position="top" :model="goodsAddForm" :rules="goodsAddRules" ref="goodsAddForm" label-width="100px"
+        class="goodsAddForm">
         <!-- 侧边栏 -->
         <el-tabs v-model="activeSteps" :tab-position="'left'" :before-leave="beforeTabLeave">
           <el-tab-pane name="0" label="基本信息">
@@ -53,12 +47,8 @@
             <!-- 商品分类复选框 -->
             <el-form-item label="商品分类" prop="goods_cat">
               <!-- 复选框开始 -->
-              <el-cascader
-                @change="selectedCateChange"
-                v-model="goodsAddForm.goods_cat"
-                :options="cateList"
-                :props="{ expandTrigger:'hover',children:'children',value:'cat_id',label:'cat_name'}"
-              ></el-cascader>
+              <el-cascader @change="selectedCateChange" v-model="goodsAddForm.goods_cat" :options="cateList"
+                :props="{ expandTrigger:'hover',children:'children',value:'cat_id',label:'cat_name'}"></el-cascader>
             </el-form-item>
           </el-tab-pane>
 
@@ -67,12 +57,7 @@
             <el-form-item v-for="(item1,index) in manyData" :key="index" :label="item1.attr_name">
               <!-- 复选按钮 -->
               <el-checkbox-group v-model="item1.attr_vals">
-                <el-checkbox
-                  v-for="(item2,index) in item1.attr_vals"
-                  :label="item2"
-                  :key="index"
-                  border
-                ></el-checkbox>
+                <el-checkbox v-for="(item2,index) in item1.attr_vals" :label="item2" :key="index" border></el-checkbox>
               </el-checkbox-group>
             </el-form-item>
           </el-tab-pane>
@@ -88,14 +73,8 @@
             <!-- on-preview处理图片预览效果 -->
             <!-- on-remove处理移除图片操作 -->
             <!-- headers为请求头对象 -->
-            <el-upload
-              :headers="headerObj"
-              :action="uploadURL"
-              :on-preview="imgPreview"
-              :on-remove="imgRemove"
-              :on-success="imgSuccess"
-              list-type="picture"
-            >
+            <el-upload :headers="headerObj" :action="uploadURL" :on-preview="imgPreview" :on-remove="imgRemove"
+              :on-success="imgSuccess" list-type="picture">
               <el-button size="small" type="primary">点击上传</el-button>
               <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
             </el-upload>
@@ -121,228 +100,271 @@
 </template>
 
 <script>
-//引入深拷贝
-import _ from 'lodash'
+  //引入深拷贝
+  import _ from 'lodash'
 
-export default {
-data(){
-  return{
-    //顶部步骤条index
-     activeSteps:'0',
-    //商品信息提交表单
-     goodsAddForm:{
-      goods_name:'',
-      goods_price:0,
-      goods_weight:0,
-      goods_number:0,
-      goods_cat:[],
-      // 图片数组
-      pics:[],
-      //商品的详情介绍（富文本编辑器）
-      goods_introduce:''
-     },
-    //表单商品信息校验规则
-     goodsAddRules:{
-      goods_name:[
-      { required: true, message: '商品名称不能为空',trigger: 'blur' },
-      { min:1,max:10, message: '商品名称在1到10字符之间',trigger: 'blur'}],
-      goods_price:[
-      { required: true, message: '商品价格不能为空',trigger: 'blur'},
+  export default {
+    data() {
+      return {
+        //顶部步骤条index
+        activeSteps: '0',
+        //商品信息提交表单
+        goodsAddForm: {
+          goods_name: '',
+          goods_price: 0,
+          goods_weight: 0,
+          goods_number: 0,
+          goods_cat: [],
+          // 图片数组
+          pics: [],
+          //商品的详情介绍（富文本编辑器）
+          goods_introduce: ''
+        },
+        //表单商品信息校验规则
+        goodsAddRules: {
+          goods_name: [{
+              required: true,
+              message: '商品名称不能为空',
+              trigger: 'blur'
+            },
+            {
+              min: 1,
+              max: 10,
+              message: '商品名称在1到10字符之间',
+              trigger: 'blur'
+            }
+          ],
+          goods_price: [{
+              required: true,
+              message: '商品价格不能为空',
+              trigger: 'blur'
+            },
 
-      {min:1,max:7, message: '商品价格必须在一位数到七位数之间',trigger: 'blur'},],
-      goods_weight:[
-      { required: true, message: '商品重量不能为空',trigger: 'blur'},
-      {min:1,max:7, message: '商品重量必须在一位数到七位数之间',trigger: 'blur'},],
-      goods_number:[
-      { required: true, message: '商品数量不能为空',trigger: 'blur'}, 
-      {min:1,max:7, message: '商品数量必须在一位数到七位数之间',trigger: 'blur'},
-     ],
-      goods_cat:[
-      { required: true, message: '请选择商品种类',trigger: 'blur'}, 
-     ]
-     },
-    //商品分类总数据
-     cateList:[],
-     //请求商品分类数据query
-     query:{
-       type:'',
-       pagenum:'',
-       pagesize:''
-     },
-     //动态参数数据
-     manyData:[],
-     //静态属性数据
-     onlyData:[]
-     ,
-    //上传文件图片地址
-      uploadURL: 'http://119.23.53.78:8888/api/private/v1/upload',
-     //图片上传组件的headers请求头对象
-      headerObj:{
-        Authorization:window.sessionStorage.getItem('token')
+            {
+              min: 1,
+              max: 7,
+              message: '商品价格必须在一位数到七位数之间',
+              trigger: 'blur'
+            },
+          ],
+          goods_weight: [{
+              required: true,
+              message: '商品重量不能为空',
+              trigger: 'blur'
+            },
+            {
+              min: 1,
+              max: 7,
+              message: '商品重量必须在一位数到七位数之间',
+              trigger: 'blur'
+            },
+          ],
+          goods_number: [{
+              required: true,
+              message: '商品数量不能为空',
+              trigger: 'blur'
+            },
+            {
+              min: 1,
+              max: 7,
+              message: '商品数量必须在一位数到七位数之间',
+              trigger: 'blur'
+            },
+          ],
+          goods_cat: [{
+            required: true,
+            message: '请选择商品种类',
+            trigger: 'blur'
+          }, ]
+        },
+        //商品分类总数据
+        cateList: [],
+        //请求商品分类数据query
+        query: {
+          type: '',
+          pagenum: '',
+          pagesize: ''
+        },
+        //动态参数数据
+        manyData: [],
+        //静态属性数据
+        onlyData: [],
+        //上传文件图片地址
+        uploadURL: 'http://119.23.53.78:8888/api/private/v1/upload',
+        //图片上传组件的headers请求头对象
+        headerObj: {
+          Authorization: window.sessionStorage.getItem('token')
+        },
+        //预览图片路径
+        imgPath: '',
+        //预览图片是否显示
+        imgIsVisiable: false
+      }
+    },
+    created() {
+      this.getCateList()
+    },
+    methods: {
+      //请求数据的方法
+      //获取商品分类数据
+      async getCateList() {
+        const {
+          data: res
+        } = await this.$http.get('categories', this.query)
+        if (res.meta.status !== 200)
+          return this.$message.error('服务器繁忙，数据获取失败')
+
+        this.cateList = res.data
       },
-      //预览图片路径
-      imgPath:'',
-      //预览图片是否显示
-      imgIsVisiable:false
-  }
-},
-created(){
- this.getCateList()
-},
-methods:{
-    //请求数据的方法
-    //获取商品分类数据
-   async  getCateList(){
-   const {data:res} = await this.$http.get('categories',this.query)
-        if(res.meta.status!==200)
-        return this.$message.error('服务器繁忙，数据获取失败')
-
-        this.cateList=res.data  
-    },
-    //根据商品id获取每条商品参数数据
-    async  getCateParams(sel){
-    const {data:res} = await this.$http.get(`categories/${this.goodsAddForm.goods_cat[2]}/attributes`,{
-      params:{
-        sel:sel
-      }
-    })
-        if(res.meta.status!==200)
-        return this.$message.error('服务器繁忙，数据获取失败')
+      //根据商品id获取每条商品参数数据
+      async getCateParams(sel) {
+        const {
+          data: res
+        } = await this.$http.get(`categories/${this.goodsAddForm.goods_cat[2]}/attributes`, {
+          params: {
+            sel: sel
+          }
+        })
+        if (res.meta.status !== 200)
+          return this.$message.error('服务器繁忙，数据获取失败')
         //将参数数据赋值给cateParams
-       
 
-  
-          //将attr_vals转化为数组形式(当数据为many时，因为双向绑定了复选框checkbox-group（需要绑定数组）；而only时使用input，不能双向绑定数组)
-           if(sel==='many'){
-             res.data.forEach(item => {
-               item.attr_vals = item.attr_vals.length===0?[]:item.attr_vals.split(' ')
-             })
-           }
-             sel==="many"?this.manyData=res.data:this.onlyData=res.data
-     
-   },
-    //监听级联选择器中选项变化
-    selectedCateChange(){
-    //只允许选择三级分类
-    if(this.goodsAddForm.goods_cat.length!==3){
-      return  this.goodsAddForm.goods_cat = []
-    }
-    },
-    //侧边栏tab按钮切换之前触发
-    beforeTabLeave(nextTab,oldTab){
-      if(oldTab==='0'&&this.goodsAddForm.goods_cat.length!==3){
-        this.$message.error('请选择商品分类后再进行下一步操作!')
-        return false
+
+
+        //将attr_vals转化为数组形式(当数据为many时，因为双向绑定了复选框checkbox-group（需要绑定数组）；而only时使用input，不能双向绑定数组)
+        if (sel === 'many') {
+          res.data.forEach(item => {
+            item.attr_vals = item.attr_vals.length === 0 ? [] : item.attr_vals.split(' ')
+          })
+        }
+        sel === "many" ? this.manyData = res.data : this.onlyData = res.data
+
+      },
+      //监听级联选择器中选项变化
+      selectedCateChange() {
+        //只允许选择三级分类
+        if (this.goodsAddForm.goods_cat.length !== 3) {
+          return this.goodsAddForm.goods_cat = []
+        }
+      },
+      //侧边栏tab按钮切换之前触发
+      beforeTabLeave(nextTab, oldTab) {
+        if (oldTab === '0' && this.goodsAddForm.goods_cat.length !== 3) {
+          this.$message.error('请选择商品分类后再进行下一步操作!')
+          return false
+        }
+
+        if (nextTab === '1') {
+          return this.getCateParams('many')
+        } else if (nextTab === '2') {
+          return this.getCateParams('only')
+        }
+      },
+      //处理图片预览效果
+      imgPreview(file) {
+        //处理图片url路径
+        // this.imgPath = file.response.data.url.replace('127.0.0.1', '119.23.53.78')
+        this.imgPath = file.response.data.url.replace('127.0.0.1', 'www.liulongbin.top')
+        this.imgIsVisiable = true
+      },
+      //处理移除图片操作
+      imgRemove(file) {
+        //移除的图片临时路径
+        // console.log(file.response.data.tmp_path);
+        //遍历goodsAddForm.pics图片列表数组,寻找图片对应项索引值然后在goodsAddForm.pics中删除
+        this.goodsAddForm.pics.forEach((item, index) => {
+          if (item.pic.indexOf(file.response.data.tmp_path)) {
+            this.goodsAddForm.pics.splice(index, 1)
+          }
+        })
+
+      },
+      // 图片上传成功
+      imgSuccess(response) {
+        //拼接获得添加成功的图片对象
+        const imgObj = {
+          pic: response.data.tmp_path
+        }
+        //将添加成功的图片对象push进pics数组
+        this.goodsAddForm.pics.push(imgObj)
+        console.log(this.goodsAddForm.pics);
+      },
+      //提交表单商品信息
+      addCateContent() {
+        this.$refs.goodsAddForm.validate(async valid => {
+          if (!valid)
+            return this.$message.error('请规范并且完整地填写表单项!')
+
+          //执行添加的业务逻辑
+          // lodash cloneDeep(obj)
+          const form = _.cloneDeep(this.goodsAddForm)
+          form.goods_cat = form.goods_cat.join(',')
+          form.attrs = []
+          //处理动态参数
+          this.manyData.forEach(item => {
+            const objParams = {
+              attr_id: item.attr_id,
+              attr_value: item.attr_vals.join(' ')
+            }
+            form.attrs.push(objParams)
+          })
+          //处理静态属性
+          this.onlyData.forEach(item => {
+            const objParams = {
+              attr_id: item.attr_id,
+              attr_value: item.attr_vals
+            }
+            form.attrs.push(objParams)
+          })
+
+          //  console.log(form.attrs);
+
+          //发起请求添加商品
+          //商品的名称必须是唯一的
+          const {
+            data: res
+          } = await this.$http.post('goods', form)
+          //  console.log(res);
+
+          if (res.meta.status !== 201)
+            return this.$message.error('添加商品失败,请勿使用重复的商品名称')
+
+          this.$message.success('商品添加成功')
+          this.$router.push('/goods')
+        })
       }
-
-      if(nextTab==='1'){
-       return this.getCateParams('many')
-      }
-       else if(nextTab==='2'){
-       return  this.getCateParams('only')
-      }
     },
-    //处理图片预览效果
-    imgPreview(file){
-      //处理图片url路径
-      // this.imgPath = file.response.data.url.replace('127.0.0.1', '119.23.53.78')
-      this.imgPath = file.response.data.url.replace('127.0.0.1', 'www.liulongbin.top')
-      this.imgIsVisiable = true
-    },
-   //处理移除图片操作
-    imgRemove(file){
-      //移除的图片临时路径
-      // console.log(file.response.data.tmp_path);
-      //遍历goodsAddForm.pics图片列表数组,寻找图片对应项索引值然后在goodsAddForm.pics中删除
-     this.goodsAddForm.pics.forEach((item,index)=>{
-        if(item.pic.indexOf(file.response.data.tmp_path)){
-          this.goodsAddForm.pics.splice(index,1)
-        }  
-     })
+  }
 
-    },
-    // 图片上传成功
-    imgSuccess(response){
-    //拼接获得添加成功的图片对象
-    const imgObj={pic:response.data.tmp_path}
-    //将添加成功的图片对象push进pics数组
-    this.goodsAddForm.pics.push(imgObj)
-     console.log(this.goodsAddForm.pics);
-    },
-    //提交表单商品信息
-    addCateContent(){
-    this.$refs.goodsAddForm.validate(async valid=>{
-      if(!valid)
-      return this.$message.error('请规范并且完整地填写表单项!')
-   
-    //执行添加的业务逻辑
-    // lodash cloneDeep(obj)
-     const form = _.cloneDeep(this.goodsAddForm)
-     form.goods_cat = form.goods_cat.join(',')
-         form.attrs=[]
-         //处理动态参数
-         this.manyData.forEach(item => {
-           const objParams = {
-             attr_id:item.attr_id,
-             attr_value:item.attr_vals.join(' ')
-           }
-          form.attrs.push(objParams)
-       })
-         //处理静态属性
-         this.onlyData.forEach(item => {
-           const objParams = {
-             attr_id:item.attr_id,
-             attr_value:item.attr_vals
-           }
-          form.attrs.push(objParams)
-       })
-      
-      //  console.log(form.attrs);
-
-       //发起请求添加商品
-       //商品的名称必须是唯一的
-       const {data:res} = await this.$http.post('goods',form)
-      //  console.log(res);
-       
-       if(res.meta.status!==201) 
-       return this.$message.error('添加商品失败,请勿使用重复的商品名称')
-
-       this.$message.success('商品添加成功')
-       this.$router.push('/goods')
-    })
-    }
-},
-}
 </script>
 
 <style lang="less" scoped>
-.el-steps {
-  margin: 28px 0!important;
-}
-
-.el-tabs {
-  .el-form-item {
-    margin:0 0 20px 0;
-    width: 30%;
+  .el-steps {
+    margin: 28px 0 !important;
   }
 
-  .el-tag {
-    margin: 15px;
+  .el-tabs {
+    .el-form-item {
+      margin: 0 0 20px 0;
+      width: 30%;
+    }
+
+    .el-tag {
+      margin: 15px;
+    }
   }
-}
 
-.el-checkbox {
-  margin: 0 10px 0 0!important;
-}
+  .el-checkbox {
+    margin: 0 10px 0 0 !important;
+  }
 
-//图片预览图
- .dialogImg {
-   width: 100%;
- }
+  //图片预览图
+  .dialogImg {
+    width: 100%;
+  }
 
- .quill-editor-addBtn {
-   margin: 20px 0;
- }
-
+  .quill-editor-addBtn {
+    margin: 20px 0;
+  }
 
 </style>
--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -11,12 +11,7 @@
     <el-card>
       <!-- 顶部输入框区域 -->
       <el-row>
-        <el-input
-          clearable
-          @clear="clearFindInput"
-          class="findGoodsInput"
-          v-model="queryInfo.query"
-        >
+        <el-input clearable @clear="clearFindInput" class="findGoodsInput" v-model="queryInfo.query">
           <el-button slot="append" icon="el-icon-search" @click="findGoodsBtn"></el-button>
         </el-input>
         <el-button type="primary" @click="goAddPage">添加商品</el-button>
@@ -43,121 +38,116 @@
             <el-button type="primary" icon="el-icon-edit" circle></el-button>
 
             <!-- 商品信息删除操作 -->
-            <el-button
-              type="danger"
-              @click="removeGoodsData(scope.row)"
-              icon="el-icon-delete"
-              circle
-            ></el-button>
+            <el-button type="danger" @click="removeGoodsData(scope.row)" icon="el-icon-delete" circle></el-button>
           </template>
         </el-table-column>
       </el-table>
 
       <!-- 分页功能 -->
-      <el-pagination
-        background
-        @size-change="pageSizeChange"
-        @current-change="PageNumChange"
-        :current-page="queryInfo.pagenum"
-        :page-sizes="[10, 20, 30, 40]"
-        :page-size="queryInfo.pagesize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="total"
-      ></el-pagination>
+      <el-pagination background @size-change="pageSizeChange" @current-change="PageNumChange"
+        :current-page="queryInfo.pagenum" :page-sizes="[10, 20, 30, 40]" :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper" :total="total"></el-pagination>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {
-  data(){
-   return{
-     queryInfo:{ 
-     //顶部商品搜索框内容双向绑定
-     query:null,
-     //当前页码
-     pagenum:1,
-     //每页显示条数
-     pagesize:20
-     },
-     //商品列表数据
-     goodsList:[],
-     //商品数据数量
-     total:0,
-   }
-  },
-  created(){
-   this.getGoodsList()
-  },
-  methods:{
-  //获取商品列表数据
-  async getGoodsList(){
-  const {data:res} = await this.$http.get('goods',{params:this.queryInfo})
+  export default {
+    data() {
+      return {
+        queryInfo: {
+          //顶部商品搜索框内容双向绑定
+          query: null,
+          //当前页码
+          pagenum: 1,
+          //每页显示条数
+          pagesize: 20
+        },
+        //商品列表数据
+        goodsList: [],
+        //商品数据数量
+        total: 0,
+      }
+    },
+    created() {
+      this.getGoodsList()
+    },
+    methods: {
+      //获取商品列表数据
+      async getGoodsList() {
+        const {
+          data: res
+        } = await this.$http.get('goods', {
+          params: this.queryInfo
+        })
 
-    if(res.meta.status!==200)
-    return this.$message.error('服务器繁忙，数据请求失败')
+        if (res.meta.status !== 200)
+          return this.$message.error('服务器繁忙，数据请求失败')
 
-    this.goodsList=res.data.goods
-    this.total=res.data.total
-  },
-  //每页多少条数据
-   pageSizeChange(sum) {
-    this.queryInfo.pagesize=sum
-    //刷新数据
-    this.getGoodsList()
+        this.goodsList = res.data.goods
+        this.total = res.data.total
       },
-  //当前是第几页
-   PageNumChange(num) {
-     this.queryInfo.pagenum=num
-     //刷新数据
-    this.getGoodsList()
-   },
-   //顶部搜索商品按钮
-   findGoodsBtn(){
-     //输入框未空或者输入字符串
-     if(this.queryInfo.query.trim()==='')
-     return this.$message.error('请输入合法数据进行查询')
-     //刷新数据
-     this.$message.success('数据查询成功')
-     this.getGoodsList()
-   },
-   //清空顶部搜索框内容
-   clearFindInput(){
-     //刷新数据
-     this.getGoodsList()
-   },
-   //删除商品数据
-  async removeGoodsData(row){
-     console.log(row.goods_id);
-     const confirmResult =await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+      //每页多少条数据
+      pageSizeChange(sum) {
+        this.queryInfo.pagesize = sum
+        //刷新数据
+        this.getGoodsList()
+      },
+      //当前是第几页
+      PageNumChange(num) {
+        this.queryInfo.pagenum = num
+        //刷新数据
+        this.getGoodsList()
+      },
+      //顶部搜索商品按钮
+      findGoodsBtn() {
+        //输入框未空或者输入字符串
+        if (this.queryInfo.query.trim() === '')
+          return this.$message.error('请输入合法数据进行查询')
+        //刷新数据
+        this.$message.success('数据查询成功')
+        this.getGoodsList()
+      },
+      //清空顶部搜索框内容
+      clearFindInput() {
+        //刷新数据
+        this.getGoodsList()
+      },
+      //删除商品数据
+      async removeGoodsData(row) {
+        console.log(row.goods_id);
+        const confirmResult = await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
-        }).catch(err=>err);     
-     
-      if(confirmResult==='cancel') 
-      return this.$message.info('取消本次删除')    
-      
-     const {data:res} = await this.$http.delete(`goods/${row.goods_id}`)
-      if(res.meta.status!=200) 
-      return this.$message.error('删除失败')
-     
-      this.$message.success('删除成功')  
-      this.getGoodsList()
-   },
-   //跳转到Add页面
-  goAddPage(){
-    this.$router.push('/goods/add')
+        }).catch(err => err);
+
+        if (confirmResult === 'cancel')
+          return this.$message.info('取消本次删除')
+
+        const {
+          data: res
+        } = await this.$http.delete(`goods/${row.goods_id}`)
+        if (res.meta.status != 200)
+          return this.$message.error('删除失败')
+
+        this.$message.success('删除成功')
+        this.getGoodsList()
+      },
+      //跳转到Add页面
+      goAddPage() {
+        this.$router.push('/goods/add')
+      }
+
+    }
   }
-  
-  } 
-}
+
 </script>
 
 <style>
-.findGoodsInput {
- width: 450px!important;
- margin-right:25px;
-}
+  .findGoodsInput {
+    width: 450px !important;
+    margin-right: 25px;
+  }
 
 </style>
